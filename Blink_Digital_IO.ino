3d928a145448ef6c173116225de5b83f54030b13
provenantsystems <167550508+provenantsystems@users.noreply.github.com>
2024-04-20 22:22:59 +0530

    Add files via upload

---
+++ Blink_Digital_IO.ino
@@ -0,0 +1,19 @@
+
+#define LED_BUILT_IN 3
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("ON");
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+  Serial.println("ON");
+
+  delay(1000);                       // wait for a second
+}
